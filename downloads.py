75e1d1614bd53f6eaf2c2d66be267234f410e266
hikkaaa <dorteel@gmail.com>
2025-07-01 11:35:13 +0200

    cleanup

--- downloads.py
+++
@@ -1,7 +0,0 @@
-from huggingface_hub import snapshot_download
-snapshot_download(
-    repo_id="mistralai/Mistral-Small-3.2-24B-Instruct-2506",
-    local_dir="mistral_small_3_2_24b",
-    resume_download=True,
-    ignore_patterns=["consolidated.safetensors"],   # optional
-)
